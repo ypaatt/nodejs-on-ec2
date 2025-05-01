d9b2785551e9aa59f1c9f6c0eb533d6118dbd5ac
Yatharth Pathak <112723864+ypaatt@users.noreply.github.com>
2025-05-01 18:08:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Analysing and designing CI/CD pipeline for Iot edge devices using raspberry pi b4'); //write a response to the client
+  res.write('Analysing and designing CI/CD pipeline for Iot edge devices using raspberry pi b4 , docker '); //write a response to the client
   res.end(); //end the response
 }).listen(4000); //the server object listens on port 4000
